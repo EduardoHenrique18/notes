f573d33a387e101fccadc689190d6e8e62a4c495
EduardoHenrique18 <eduardo.henrique204@gmail.com>
2020-04-19 16:09:26 -0300

    change response object

--- src/utils/httpResponse.js
+++ src/utils/httpResponse.js
@@ -8,43 +8,40 @@ module.exports = class HttpResponse {
   ok (data) {
     return this.res.send({
       statusCode: 200,
-      data
+      data,
+      error: ''
     })
   }
 
   badRequest (error) {
     return this.res.send({
       statusCode: 400,
-      data: {
-        error: error.message
-      }
+      data: {},
+      error: error.message
     })
   }
 
   conflictError (error) {
     return this.res.send({
       statusCode: 409,
-      data: {
-        error: error.message
-      }
+      data: {},
+      error: error.message
     })
   }
 
   unauthorizedError (error) {
     return this.res.send({
       statusCode: 401,
-      data: {
-        error: error.message
-      }
+      data: {},
+      error: error.message
     })
   }
 
   serverError () {
     return this.res.send({
       statusCode: 500,
-      data: {
-        error: new ServerError().message
-      }
+      data: {},
+      error: new ServerError().message
     })
   }
 }
